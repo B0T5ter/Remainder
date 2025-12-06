9f057309dd3a4a8a2698cc8cfa94a2360fd862a7
Filip <targosz.f@gmail.com>
2025-12-06 18:44:50 +0100

    dasd

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 # wysyłanie zadania
 task = {"name": "Nauka Tkinter", "repeat": "Every x"}
-res = requests.post("http://192.168.50.200:5000/get_tasks", json=task)
+res = requests.post("http://192.168.50.200:5000/add_task", json=task)  # <--- zmieniony endpoint
 print(res.json())
 
 # pobieranie wszystkich zadań
